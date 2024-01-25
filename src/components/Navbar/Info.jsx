3862cc5d375cdd45ccd5e85443a6cebe22f25f9d
Piotr Kaczorowski <piotr.kaczorowski@gorrion.io>
2024-01-25 15:15:10 +0100

    Finished instruction

--- src/components/Navbar/Info.jsx
+++ src/components/Navbar/Info.jsx
@@ -16,11 +16,11 @@ const Info = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="z-[200] bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="z-[200] text-gray-950 text-[15px] overflow-auto data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[50vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Content className="z-[200] text-gray-950 text-[15px] overflow-auto data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-gray-950 m-0 text-[17px] font-medium">
             Crazy Blocks - Instruction
           </Dialog.Title>
-          <Dialog.Description className="text-gray-950 mt-[10px] mb-2 text-[15px] leading-normal">
+          <Dialog.Description className="text-gray-950 mt-[10px] mb-2 text-[16px] leading-[18px]">
             Crazy Blocks is an application that allows user to create, play with
             and export block diagrams.
           </Dialog.Description>
@@ -28,23 +28,67 @@ const Info = () => {
             <div>Blocks:</div>
             <div className="flex items-center gap-1">
               <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
-              <div>Start Block</div>
+              <div>Start Block {`(limit of one, starts block chart)`}</div>
             </div>
             <div className="flex items-center gap-1">
               <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
-              <div>Data Block</div>
+              <div>Data Block {`(declaring variables)`}</div>
             </div>
             <div className="flex items-center gap-1">
               <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
-              <div>Process Block</div>
+              <div>Process Block {`(processing variables)`}</div>
             </div>
             <div className="flex items-center gap-1">
               <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
-              <div>Decision Block</div>
+              <div>Decision Block {`(conditional statements)`}</div>
             </div>
             <div className="flex items-center gap-1">
               <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
-              <div>End Block</div>
+              <div>End Block {`(limit of one, ends block chart)`}</div>
+            </div>
+          </div>
+          <div className="flex mt-2 flex-col">
+            <div>Available commands:</div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Declaring variables {`(i = 8)`}</div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Declaring arrays {`(arr = [1,2,3,4,5])`}</div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Incrementation etc. {`(i++; i--; etc.)`}</div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Assigning to variable {`i = 5;`}</div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>
+                Assigning array length or value {`(i = arr.length, i = arr[2])`}
+              </div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Assigning to an array {`(arr[3] = 7)`}</div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Printing value {`(print Variable i equals {i})`}</div>
+            </div>
+          </div>
+          <div className="flex mt-2 flex-col">
+            <div>Rules:</div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Every instruction needs to end with semicolon {`(;)`}</div>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="ml-1 rounded-full w-[7px] h-[7px] bg-gray-900"></div>
+              <div>Only one instruction allowed in decision block</div>
             </div>
           </div>
           <Dialog.Close asChild>
